c79cbb47202a2148b944756ced1443e952bdb1e7
Sorah Fukumori <her@sorah.jp>
2024-04-26 03:10:00 +0900

    d/ch

--- src/prepare.sh
+++ src/prepare.sh
@@ -8,6 +8,7 @@ DIST=$(lsb_release -sc)
 sed -i -e "s/ITAMAE_VERSION/${ITAMAE_VERSION}/g" debian/changelog
 sed -i -e "s/PKGREV/${PKGREV}/g" debian/changelog
 sed -i -e "s/DIST/${DIST}/g" debian/changelog
+sed -i -e "s/PKGDATE/${PKGDATE}/g" debian/changelog
 
 if [ "_${DIST}" = "_trusty" -o "_${DIST}" = "_xenial" ]; then
   echo 9 > debian/compat
